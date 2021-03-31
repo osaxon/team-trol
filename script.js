16d3c89d5b3e34fa97ea110b8bb9d722355dd812
Ross White <rosswhite@outlook.com>
2021-03-31 21:13:44 +0100

    Setting up initial script and linking

---
+++ script.js
@@ -0,0 +1,24 @@
+//background remover API key
+// BG_API_KEY = "9c8057038dmsh60c7edf2f2e2a75p109df1jsnae560dc057db"
+
+
+//Background Remover API fetch request 
+// fetch("https://background-remover.p.rapidapi.com/remove-background", {
+// 	"method": "POST",
+// 	"headers": {
+// 		"content-type": "application/json",
+// 		"x-rapidapi-key": API_KEY,
+// 		"x-rapidapi-host": "background-remover.p.rapidapi.com"
+// 	},
+// 	"body": {
+// 		"imageBase64": ""
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+console.log("hello world");
