38f6b9ac1739e6a1d464dd2012e69a7f05d96bee
Oliver Saxon <olisaxon@Olivers-Mac-mini.local>
2021-04-03 11:15:23 +0100

    FEAT: Created Canvas element and image uploader

--- script.js
+++ script.js
@@ -34,3 +34,20 @@ console.log("Hello World");
         //Destination image first, cropped user image second
     //Button is displayed to click when user is happy with final image
         //Button click saves final image, making it available to download
+
+const $imgUploader = $('.imgUploader');
+
+const canvas = document.getElementById('#usrImgCanvas')
+const ctx = canvas.getContext('2d');
+
+function loadImage(){
+    var img = new Image();
+    img.onload = drawImg;
+    img.src = URL.createObjectURL(this.files[0]);
+};
+
+function drawImg() {
+    canvas.width = this.width;
+    canvas.height = this.height;
+    ctx.drawImage(this, 0,0);
+};
